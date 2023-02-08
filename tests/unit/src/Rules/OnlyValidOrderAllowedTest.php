d951777db32ee042900ef0e38899b771b01bfeb7
Abdul Wahab Qureshi <its.inevitable@hotmail.com>
2023-02-08 12:07:23 +0000

    Package made compatible with php 7.1

--- tests/unit/src/Rules/OnlyValidOrderAllowedTest.php
+++ tests/unit/src/Rules/OnlyValidOrderAllowedTest.php
@@ -8,7 +8,7 @@ final class OnlyValidOrderAllowedTest extends TestCase
 {
     public function setUp(): void
     {
-        $featureFileContents = $this->createStub(Entities\FeatureFileContents::class);
+        $featureFileContents = $this->createMock(Entities\FeatureFileContents::class);
         $featureFileContents->filePath = './abc.feature';
 
         $this->testObject = new Rules\OnlyValidOrderAllowed();
@@ -19,7 +19,7 @@ final class OnlyValidOrderAllowedTest extends TestCase
     {
         $collection = $this->createMock(Entities\OutcomeCollection::class);
 
-        $scenario = $this->createStub(Entities\Scenario::class);
+        $scenario = $this->createMock(Entities\Scenario::class);
 
         $scenario->method('getActiveSteps')
             ->willReturn([]);
@@ -36,33 +36,33 @@ final class OnlyValidOrderAllowedTest extends TestCase
     public function testApplyOnScenarioAllGood()
     {
         $collection = $this->createMock(Entities\OutcomeCollection::class);
-        $scenario = $this->createStub(Entities\Scenario::class);
+        $scenario = $this->createMock(Entities\Scenario::class);
 
-        $step = $this->createStub(Entities\Step::class);
+        $step = $this->createMock(Entities\Step::class);
         $step->lineNumber = 50;
         $step->title = '   Given I am doing this test';
         $step->trimmedTitle = 'Given I am doing this test';
         $step->method('getKeyword')->willReturn('given');
 
-        $step2 = $this->createStub(Entities\Step::class);
+        $step2 = $this->createMock(Entities\Step::class);
         $step2->lineNumber = 52;
         $step2->title = '   When I do something';
         $step2->trimmedTitle = trim($step2->title);
         $step2->method('getKeyword')->willReturn('when');
 
-        $step3 = $this->createStub(Entities\Step::class);
+        $step3 = $this->createMock(Entities\Step::class);
         $step3->lineNumber = 53;
         $step3->title = '   And I do something';
         $step3->trimmedTitle = trim($step3->title);
         $step3->method('getKeyword')->willReturn('and');
 
-        $step4 = $this->createStub(Entities\Step::class);
+        $step4 = $this->createMock(Entities\Step::class);
         $step4->lineNumber = 54;
         $step4->title = '   Then I do something';
         $step4->trimmedTitle = trim($step4->title);
         $step4->method('getKeyword')->willReturn('then');
 
-        $step5 = $this->createStub(Entities\Step::class);
+        $step5 = $this->createMock(Entities\Step::class);
         $step5->lineNumber = 55;
         $step5->title = '   But I do something';
         $step5->trimmedTitle = trim($step5->title);
@@ -89,27 +89,27 @@ final class OnlyValidOrderAllowedTest extends TestCase
     public function testApplyOnScenarioAllGoodStartWithWhen()
     {
         $collection = $this->createMock(Entities\OutcomeCollection::class);
-        $scenario = $this->createStub(Entities\Scenario::class);
+        $scenario = $this->createMock(Entities\Scenario::class);
 
-        $step2 = $this->createStub(Entities\Step::class);
+        $step2 = $this->createMock(Entities\Step::class);
         $step2->lineNumber = 52;
         $step2->title = '   When I do something';
         $step2->trimmedTitle = trim($step2->title);
         $step2->method('getKeyword')->willReturn('when');
 
-        $step3 = $this->createStub(Entities\Step::class);
+        $step3 = $this->createMock(Entities\Step::class);
         $step3->lineNumber = 53;
         $step3->title = '   And I do something';
         $step3->trimmedTitle = trim($step3->title);
         $step3->method('getKeyword')->willReturn('and');
 
-        $step4 = $this->createStub(Entities\Step::class);
+        $step4 = $this->createMock(Entities\Step::class);
         $step4->lineNumber = 54;
         $step4->title = '   Then I do something';
         $step4->trimmedTitle = trim($step4->title);
         $step4->method('getKeyword')->willReturn('then');
 
-        $step5 = $this->createStub(Entities\Step::class);
+        $step5 = $this->createMock(Entities\Step::class);
         $step5->lineNumber = 55;
         $step5->title = '   But I do something';
         $step5->trimmedTitle = trim($step5->title);
@@ -135,15 +135,15 @@ final class OnlyValidOrderAllowedTest extends TestCase
     public function testApplyOnScenario()
     {
         $collection = $this->createMock(Entities\OutcomeCollection::class);
-        $scenario = $this->createStub(Entities\Scenario::class);
+        $scenario = $this->createMock(Entities\Scenario::class);
 
-        $step = $this->createStub(Entities\Step::class);
+        $step = $this->createMock(Entities\Step::class);
         $step->lineNumber = 50;
         $step->title = '   Given I am doing this test';
         $step->trimmedTitle = 'Given I am doing this test';
         $step->method('getKeyword')->willReturn('given');
 
-        $step2 = $this->createStub(Entities\Step::class);
+        $step2 = $this->createMock(Entities\Step::class);
         $step2->lineNumber = 51;
         $step2->title = '   Then I do something';
         $step2->trimmedTitle = 'Then I do something';
@@ -171,15 +171,15 @@ final class OnlyValidOrderAllowedTest extends TestCase
     public function testApplyOnScenarioDifferent()
     {
         $collection = $this->createMock(Entities\OutcomeCollection::class);
-        $scenario = $this->createStub(Entities\Scenario::class);
+        $scenario = $this->createMock(Entities\Scenario::class);
 
-        $step = $this->createStub(Entities\Step::class);
+        $step = $this->createMock(Entities\Step::class);
         $step->lineNumber = 50;
         $step->title = '   And I am doing this test';
         $step->trimmedTitle = trim($step->title);
         $step->method('getKeyword')->willReturn('and');
 
-        $step2 = $this->createStub(Entities\Step::class);
+        $step2 = $this->createMock(Entities\Step::class);
         $step2->lineNumber = 51;
         $step2->title = '   Then I do something';
         $step2->trimmedTitle = trim($step2->title);
